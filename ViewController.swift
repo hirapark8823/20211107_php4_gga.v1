543029f60f2049047b6b26fc4a5ba49732ef3df2
hirapark8823 <90318741+hirapark8823@users.noreply.github.com>
2021-11-12 17:35:45 +0900

    はじめてのすいふと

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  solovel_practice
+//
+//  Created by 平本尚寛 on 2021/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
